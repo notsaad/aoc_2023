3115aade313adafb5e443c758458538c9e22706c
notsaad <saadmazharr@gmail.com>
2025-10-22 18:11:16 -0400

    setup templating

---
+++ template.cpp
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+
+// basic template for reading input in c++
+int main() {
+    std::string line;
+    while (getline(std::cin, line)) {
+        std::cout << "Read: " << line << '\n';
+    }
+}
